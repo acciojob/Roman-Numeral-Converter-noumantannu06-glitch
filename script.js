f4437d5e661f4e4a0d37871d828de27c9a9bca18
Md Nouman <noumantannu06@gmail.com>
2026-03-24 22:20:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
-function intToRoman(num) {
-    // Define the Roman numeral symbols and their values
-    const romanSymbols = [
-        ['M', 1000],
-        ['CM', 900],
-        ['D', 500],
-        ['CD', 400],
-        ['C', 100],
-        ['XC', 90],
-        ['L', 50],
-        ['XL', 40],
-        ['X', 10],
-        ['IX', 9],
-        ['V', 5],
-        ['IV', 4],
-        ['I', 1]
-    ];
-    
-    let result = '';
+        function intToRoman(num) {
+            // Define the Roman numeral symbols and their values
+            const romanSymbols = [
+                ['M', 1000],
+                ['CM', 900],
+                ['D', 500],
+                ['CD', 400],
+                ['C', 100],
+                ['XC', 90],
+                ['L', 50],
+                ['XL', 40],
+                ['X', 10],
+                ['IX', 9],
+                ['V', 5],
+                ['IV', 4],
+                ['I', 1]
+            ];
 
-    for (let [roman, value] of romanSymbols) {
-        while (num >= value) {
-            result += roman; 
-            num -= value;    
+            let result = '';
+
+            for (let [roman, value] of romanSymbols) {
+                while (num >= value) {
+                    result += roman;
+                    num -= value;
+                }
+            }
+
+            return result;
         }
-    }
 
-    return result; 
-}
-
-// Add event listener to the button
-document.getElementById('convertBtn').addEventListener('click', function() {
-    const inputNum = parseInt(document.getElementById('numberInput').value);
-    const result = intToRoman(inputNum);
-    document.getElementById('result').innerText = result;
-});
+        // Add event listener to the button
+        document.getElementById('convertBtn').addEventListener('click', function () {
+            const inputNum = parseInt(document.getElementById('numberInput').value);
+            if (isNaN(inputNum) || inputNum < 0 || inputNum > 100000) {
+                document.getElementById('result').innerText = 'Please enter a valid number between 0 and 100000.';
+            } else {
+                const result = intToRoman(inputNum);
+                document.getElementById('result').innerText = result;
+            }
+        });
