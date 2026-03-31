e943bdcb66a3f5f690432b47078b06926da79a2d
Md Nouman <noumantannu06@gmail.com>
2026-03-31 21:06:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,42 +1,23 @@
 function intToRoman(num) {
             // Define the Roman numeral symbols and their values
-            const romanSymbols = [
-                ['M', 1000],
-                ['CM', 900],
-                ['D', 500],
-                ['CD', 400],
-                ['C', 100],
-                ['XC', 90],
-                ['L', 50],
-                ['XL', 40],
-                ['X', 10],
-                ['IX', 9],
-                ['V', 5],
-                ['IV', 4],
-                ['I', 1]
-            ];
-
-            let result = '';
-
-            // Iterate over the symbols
-            for (let [roman, value] of romanSymbols) {
-                // Subtract the value from num and append the symbol to result
-                while (num >= value) {
-                    result += roman;
-                    num -= value;
-                }
-            }
-
-            return result; 
-        }
-
-        // Add event listener to the button
-        document.getElementById('convertBtn').addEventListener('click', function () {
-            const inputNum = parseInt(document.getElementById('numberInput').value);
-            if (isNaN(inputNum) || inputNum < 0 || inputNum > 100000) {
-                document.getElementById('result').innerText = 'Please enter a valid number between 0 and 100000.';
-            } else {
-                const result = intToRoman(inputNum);
-                document.getElementById('result').innerText = result;
-            }
-        });
+const symbols = [
+    ['M', 1000], 
+    ['D', 500], 
+    ['C', 100], 
+    ['L', 50], 
+    ['X', 10], 
+    ['V', 5], 
+    ['I', 1]
+  ];
+  
+  let result = '';
+  
+  for (let [symbol, value] of symbols) {
+    while (num >= value) {
+      result += symbol;
+      num -= value;
+    }
+  }
+  
+  return result;
+}
